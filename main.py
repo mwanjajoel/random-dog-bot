9d6540c6bcac756d422e1be5670202fb97f827b7
mwanjajoel <joelsilverworks@gmail.com>
2020-03-08 22:20:20 +0300

    updated main file and added dot-env

---
+++ main.py
@@ -0,0 +1,23 @@
+# import the libraries
+from telegram.ext import Updater, CommandHandler
+import requests
+import re
+
+# access the API and get the image URL
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()
+    image_url = contents['url']
+
+    return image_url
+
+# function to return the image to the sender 
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater
+
+
+
